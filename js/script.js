c3a123a53849df69c1fdb4081619e253fdf87c98
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-14 22:54:14 -0300

    css animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// api
+const url = 'https://tarot-yn.herokuapp.com/cards'
+
+function getCards() {
+let response = fetch(url)
+    .then(response => {
+
+      if (!response.ok) {
+        console.log('Failed to fetch');
+      }
+
+      if (response.status === 404) {
+        console.log('Error 404');
+      }
+      return response.json();
+    });
+}
+
+console.log(getCards());
+
+// modal
 const modalContainer = document.getElementById('modalContainer');
 const card = document.getElementById('card');
 const closeButton = document.getElementById('closeButton');
@@ -5,7 +26,8 @@ const playAgain = document.getElementById('again');
 
 // Show the modalContainer when click on card
 card.onclick = function() {
-    card.style.animation = "flip 1s linear";
+    this.style.animation = '';
+    setTimeout(() => {card.style.animation = "flip 1s linear";}, 500);
     setTimeout(() => {modalContainer.style.display = "block";}, 750);
     setTimeout(() => {modalContainer.style.opacity = 1;}, 1000);
 
@@ -18,7 +40,7 @@ closeButton.onclick = function() {
 }
 
 playAgain.onclick = function(e){
-    e.stopPropagation();
+    e.preventDefault();
     modalContainer.style.opacity = 0;
     setTimeout(() => {modalContainer.style.display = 'none'}, 900);
   }
