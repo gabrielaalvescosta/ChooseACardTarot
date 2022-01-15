62647f9044dc7be5643c0c2837f650928a0301e4
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-15 02:58:55 -0300

    Mudança Título API

--- js/app.js
+++ js/app.js
@@ -1,37 +1,31 @@
-// api
-const url = 'https://tarot-yn.herokuapp.com/cards'
-
-function getCards() {
-let response = fetch(url)
-    .then(response => {
-
-      if (!response.ok) {
-        console.log('Failed to fetch');
-      }
-
-      if (response.status === 404) {
-        console.log('Error 404');
-      }
-      return response.json();
-    });
-}
-
-console.log(getCards());
-
 // modal
 const modalContainer = document.getElementById('modalContainer');
 const card = document.getElementById('card');
 const closeButton = document.getElementById('closeButton');
 const playAgain = document.getElementById('again');
 
+function getCards(){
+  fetch('https://tarot-yn.herokuapp.com/cards')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          return data;
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+  }
+
 // Show the modalContainer when click on card
 card.onclick = function() {
     this.style.animation = '';
+    getCards();
     setTimeout(() => {card.style.animation = "flip 1s linear";}, 500);
     setTimeout(() => {modalContainer.style.display = "block";}, 750);
     setTimeout(() => {modalContainer.style.opacity = 1;}, 1000);
-
-  }
+}
 
 // Close modalContainer when click in "x"
 closeButton.onclick = function() {
@@ -43,5 +37,5 @@ playAgain.onclick = function(e){
     e.preventDefault();
     modalContainer.style.opacity = 0;
     setTimeout(() => {modalContainer.style.display = 'none'}, 900);
-  }
+}
   
