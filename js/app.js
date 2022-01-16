f3b63ccf26791cc68641ce4204cb835526a1d199
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-16 01:30:25 -0300

    Functions / adding styles

--- js/app.js
+++ js/app.js
@@ -8,45 +8,62 @@ const playAgain = document.getElementById('again');
 
 
 
-function getCards(){
+async function getCards(cardNumber){ 
 
-  // random number card
-  const min = Math.ceil(0);
-  const max = Math.floor(21);
-  let cardNumber = Math.floor(Math.random() * (max - min)) + min;
- 
+  const res = await fetch(url)
+  const dados = await res.json();
 
-  fetch(url)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          console.log(data.cartas);
-          document.getElementById('card-name').innerHTML = data.cartas[cardNumber].card_name;
-          document.getElementById('explanation').innerHTML = data.cartas[cardNumber].explanation;
-          document.getElementById('yes-or-no').innerHTML = data.cartas[cardNumber].yes_or_no;
+  document.getElementById('card-name').innerHTML = dados.cartas[cardNumber].card_name;
+  document.getElementById('explanation').innerHTML = dados.cartas[cardNumber].explanation;
+  document.getElementById('yes-or-no').innerHTML = dados.cartas[cardNumber].yes_or_no;
 
-          const image = document.createAttribute("src");
-          image.value = data.cartas[cardNumber].url;
-          document.getElementById('photo').setAttributeNode(image);
-          
-          console.log(data.cartas[cardNumber].url);
+  const image = document.createAttribute("src");
+  image.value = dados.cartas[cardNumber].url;
+  document.getElementById('photo').setAttributeNode(image);
 
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  return dados;
+
+/*                .then(function(response) {
+                  return response.json();
+                })
+                .then(function(data) {
+                  console.log(data.cartas);
+                  document.getElementById('card-name').innerHTML = data.cartas[cardNumber].card_name;
+                  document.getElementById('explanation').innerHTML = data.cartas[cardNumber].explanation;
+                  document.getElementById('yes-or-no').innerHTML = data.cartas[cardNumber].yes_or_no;
+
+                  const image = document.createAttribute("src");
+                  image.value = data.cartas[cardNumber].url;
+                  document.getElementById('photo').setAttributeNode(image);
+                  
+                  console.log(data.cartas[cardNumber].url);
+
+                })
+                .catch((error) => {
+                  console.error(error);
+                }); */
+
 }
 
 // Show the modalContainer when click on card
 for (let i = 0; i < card.length; i++) {
 
-card[i].onclick = function() {
+card[i].onclick = async function() {
   this.style.animation = '';
-  getCards();
+
+    // random number card
+    const min = Math.ceil(0);
+    const max = Math.floor(21);
+    let cardNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  const dados = await getCards(cardNumber);
   
-  setTimeout(() => {card[i].style.animation = "flip 1s linear";}, 500);
-  setTimeout(() => {modalContainer.style.display = "block";}, 750);
+  setTimeout(() => {
+    card[i].style.animation = "flip 1s linear";}, 500);
+  setTimeout(() => {
+    modalContainer.style.display = "block";
+    this.style.backgroundImage = `url('${dados.cartas[cardNumber].url}')`;
+}, 750);
   setTimeout(() => {modalContainer.style.opacity = 1;}, 1000);
 }
 
@@ -71,6 +88,9 @@ closeButton.onclick = function() {
 playAgain.onclick = function(e){
     e.preventDefault();
     modalContainer.style.opacity = 0;
-    setTimeout(() => {modalContainer.style.display = 'none'}, 900);
+    setTimeout(() => {
+      modalContainer.style.display = 'none';
+      modalContainer.style.backgroundImage = `url('${dados.cartas[cardNumber].url}')`;
+  }, 900);
 }
   
