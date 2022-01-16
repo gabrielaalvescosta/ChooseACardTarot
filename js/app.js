75f7a5f27901d8f863b48b591826b56bff1a0c4e
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-16 16:24:29 -0300

    Update: flip cards

--- js/app.js
+++ js/app.js
@@ -22,39 +22,19 @@ async function getCards(cardNumber){
 
   return dados;
 
-/*                .then(function(response) {
-                  return response.json();
-                })
-                .then(function(data) {
-                  console.log(data.cartas);
-                  document.getElementById('card-name').innerHTML = data.cartas[cardNumber].card_name;
-                  document.getElementById('explanation').innerHTML = data.cartas[cardNumber].explanation;
-                  document.getElementById('yes-or-no').innerHTML = data.cartas[cardNumber].yes_or_no;
-
-                  const image = document.createAttribute("src");
-                  image.value = data.cartas[cardNumber].url;
-                  document.getElementById('photo').setAttributeNode(image);
-                  
-                  console.log(data.cartas[cardNumber].url);
-
-                })
-                .catch((error) => {
-                  console.error(error);
-                }); */
-
 }
+
+// random number card
+  const min = Math.ceil(0);
+  const max = Math.floor(21);
+  let cardNumber = Math.floor(Math.random() * (max - min)) + min;
+
 
 // Show the modalContainer when click on card
 for (let i = 0; i < card.length; i++) {
 
 card[i].onclick = async function() {
-  this.style.animation = '';
-
-    // random number card
-    const min = Math.ceil(0);
-    const max = Math.floor(21);
-    let cardNumber = Math.floor(Math.random() * (max - min)) + min;
-
+    this.style.animation = '';
     const dados = await getCards(cardNumber);
   
   setTimeout(() => {
@@ -71,15 +51,7 @@ card[i].onclick = async function() {
 
 }
 
-/* card.onclick = function() {
-    this.style.animation = '';
-    getCards();
-    
-    setTimeout(() => {card.style.animation = "flip 1s linear";}, 500);
-    setTimeout(() => {modalContainer.style.display = "block";}, 750);
-    setTimeout(() => {modalContainer.style.opacity = 1;}, 1000);
-}
- */
+
 
 // Close modalContainer when click in "x"
 closeButton.onclick = function() {
@@ -87,13 +59,29 @@ closeButton.onclick = function() {
     setTimeout(() => {modalContainer.style.display = 'none'}, 900);
 }
 
+
+function flipCards() {
+
+}
+
+// play again button
 playAgain.onclick = function(e){
+
     e.preventDefault();
     modalContainer.style.opacity = 0;
-    card[i].style.animation = "flip 1s linear";
+    const backgroundCard = 'url(https://raw.githubusercontent.com/gabrielaalvescosta/ChooseACardTarot/main/assets/card.png)';
+    let cardClose = document.getElementsByClassName('main__tarot-card');
+
+    for(let j = 0; j < cardClose.length; j++) {
+      cardClose[j].style.filter = "grayscale(0%)";
+      cardClose[j].style.backgroundImage = backgroundCard;
+      cardClose[j].style.animation = "flip 1s linear";
+    }
+    
     setTimeout(() => {
       modalContainer.style.display = 'none';
-      modalContainer.style.backgroundImage = `url('${dados.cartas[cardNumber].url}')`;
-  }, 900);
+  }, 400);
+
+
 }
   
